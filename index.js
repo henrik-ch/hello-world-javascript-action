f5d38d4f13c6c2ba296996015fd6bcb2001651c4
Henrik Karlsson <i97henka@gmail.com>
2021-07-16 22:08:43 +0200

    num conversion prices.

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ try {
     const limitPrice = core.getInput('limit-price');
     const currentPrice = core.getInput('current-price');
     const previousPrice = core.getInput('previous-price');
+    const numLimitPrice = parseFloat(limitPrice);
+    const numCurrentPrice = parseFloat(currentPrice);
+    const numPreviousPrice = parseFloat(previousPrice);
+
     console.log(`Hello ${nameToGreet}!`);
     console.log(`Limit price: ${limitPrice}`);
     console.log(`Current price: ${currentPrice}`);
     console.log(`Previous price: ${previousPrice}`);
+
+    console.log(`num Limit price: ${numLimitPrice}`);
+    console.log(`num Current price: ${numCurrentPrice}`);
+    console.log(`num Previous price: ${numPreviousPrice}`);
+
+
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     core.setOutput("limit-event", true);
